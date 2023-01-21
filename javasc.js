0310e5ec65bdfe0759211ab9c0dc41fb1a70f11c
nadawaja <122678518+nadawaja@users.noreply.github.com>
2023-01-21 15:42:10 +0300

    Add files via upload

---
+++ javasc.js
@@ -0,0 +1,20 @@
+window.alert("Hello");
+    var data = ["Alma","22","5"];
+    let name= data[1];
+    data[2]= 30;
+    write.document("HI");
+    let length = data.length;
+    document.write(data[length-1]);
+    data.push(40);
+    let salary =500;
+    const employee = {id:1, name:'hala', sal:5000};
+    salary = employee.sal;
+    const data2 = {
+        firstName: "rory",
+        lastName: "rain",
+        id: 5959,
+        fullName: function() {
+            return this.firstName + "" + this.lastName;
+            }
+    };
+    document.write(salary); 
---
+++ process.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+welcome <?php echo $_POST["name"];?><br>
+your email address is:<?php echo $_POST["email"];?>
+
+</body>
+</html>
